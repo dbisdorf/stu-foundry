b671d8577f4672ca3dbf2f0db440bf0a13c7922a
dbisdorf <dbisdorf@gmail.com>
2020-11-07 09:15:28 -0500

    Use hooks instead of inheritance.

--- modules/stu.js
+++ modules/stu.js
@@ -1,11 +1,20 @@
 import { SaveTheUniverseSheet } from "./actor-sheet.js";
-import { SaveTheUniverseActor } from "./actor.js";
 
 Hooks.once("init", async function() {
 	console.log("Initializing Save the Universe System");
 
-	CONFIG.Actor.entityClass = SaveTheUniverseActor;
-
 	Actors.unregisterSheet("core", ActorSheet);
 	Actors.registerSheet("stu", SaveTheUniverseSheet, { makeDefault: true });
+});
+
+Hooks.on("createOwnedItem", (parentEntity, childData, options, userId) => {
+    parentEntity.items.forEach(i => {
+		if ((childData.type === i.data.type) && (i.data.name != childData.name)) {
+			parentEntity.deleteOwnedItem(i.id);
+		}
+	});	
+    parentEntity.update({
+		"data.equipment": childData.data.equipment
+	});	
+	return true;
 });
